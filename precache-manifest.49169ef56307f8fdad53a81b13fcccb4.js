59e5840fcf5225acc87ba75e02d7947e493e9b9b
Tejodeep <tejodeepmitraroy2002@gmail.com>
2022-07-29 10:11:32 +0530

    Textutils build

---
+++ precache-manifest.49169ef56307f8fdad53a81b13fcccb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5904cac45c5e214c93c0",
+    "url": "/TextUtils/static/css/main.9b23bf4e.chunk.css"
+  },
+  {
+    "revision": "5904cac45c5e214c93c0",
+    "url": "/TextUtils/static/js/main.5904cac4.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "f3e589d65396df969814f195a822347c",
+    "url": "/TextUtils/index.html"
+  }
+];
